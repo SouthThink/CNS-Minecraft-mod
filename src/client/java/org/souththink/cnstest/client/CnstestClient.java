e31630b67cf403079756d2520b986ecda2312ec3
喃巷 <south_think@qq.com>
2025-04-26 15:29:58 +0800

    首次提交

---
+++ src/client/java/org/souththink/cnstest/client/CnstestClient.java
@@ -0,0 +1,10 @@
+package org.souththink.cnstest.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CnstestClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/souththink/cnstest/Cnstest.java
@@ -0,0 +1,10 @@
+package org.souththink.cnstest;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cnstest implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
